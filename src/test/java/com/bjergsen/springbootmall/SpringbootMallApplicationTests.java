0b9efc0fb4ed53993f16e3829924803390bdc884
Bjergsen <ooolin321@gmail.com>
2022-05-05 17:38:50 +0800

    init Spring Boot Project

---
+++ src/test/java/com/bjergsen/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjergsen.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
